351abcfe71853438e605dcfd6903af3f75eeada1
Anthony <anthonywlynn2002@gmail.com>
2025-07-22 00:51:31 -0400

    test: add mathUtil tests

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,2 @@
+selene ./src
+stylua .
--- src/util/mathUtil.lua
+++ src/util/mathUtil.lua
@@ -1,7 +1,7 @@
 local mathUtil = {}
 
 function mathUtil.quadraticEquation(a: number, b: number, c: number): { min: number, max: number }
-	assert(a ~= 0, "Value a cannot be equal to 0")
+	assert(a ~= 0, "Value 'a' cannot be equal to 0")
 
 	local sqrt = math.sqrt(b * b - 4 * a * c)
 	local inverseA2 = 1 / (a * 2)
@@ -13,6 +13,8 @@ function mathUtil.quadraticEquation(a: number, b: number, c: number): { min: num
 end
 
 function mathUtil.isInRange(val: number, min: number, max: number): boolean
+	assert(min <= max, `The min: {min}, is greater than max: {max}`)
+
 	if val < min then
 		return false
 	end
